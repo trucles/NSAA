a9d272a167f51e70f3ff25808e61311811053764
trucles <trucles@yahoo.com>
2020-08-24 22:08:05 -0500

    Added pricing page

--- js/bootshape.js
+++ js/bootshape.js
@@ -1,3 +1,48 @@
 $(function() {
     $('.carousel').carousel('cycle');
+
+	$(".navbar-toggle").blur(function(event){
+	var screenWidth = window.innerWidth;
+	if (screenWidth < 768) {
+		$(".navbar-collapse").collapse('hide');
+		}
+	});
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+var det = document.getElementsByClassName("btn");
+var j;
+
+for (j = 0; j < det.length; j++) {
+  det[j].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
